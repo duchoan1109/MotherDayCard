433ff0971d0de10f607499aea4b8c81d7694923a
Nguyen Hoan <93896421+duchoan1109@users.noreply.github.com>
2025-05-10 19:54:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,23 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 const additionalMessage = document.querySelector(".additional-message");
 
-// Kiểm tra nếu các phần tử tồn tại trong DOM
-if (yesBtn && noBtn && question && gif) {
-  yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Tới công chuyện với bà mày rồi =)))))";
-    gif.src = "https://media.giphy.com/media/MFkTITj69pMOPlbfeX/giphy.gif";
-    // Ẩn cả hai nút "Yes" và "No"
-    yesBtn.style.display = "none";
-    noBtn.style.display = "none";
-  });
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Tới công chuyện với bà mày rồi =)))))";
+  gif.src =
+    "https://media.giphy.com/media/MFkTITj69pMOPlbfeX/giphy.gif";
+  // Ẩn cả hai nút "Yes" và "No"
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+});
 
-  noBtn.addEventListener("mouseover", () => {
-    const noBtnRect = noBtn.getBoundingClientRect();
-    const maxX = window.innerWidth - noBtnRect.width;
-    const maxY = window.innerHeight - noBtnRect.height;
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
-  });
-} else {
-  console.error("Một hoặc nhiều phần tử không tồn tại trong DOM");
-}
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
